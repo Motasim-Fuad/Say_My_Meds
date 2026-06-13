38ea10560b5a9411fb102651a5f06aaac2021892
Motasim Fuad <motasimfuad@Betopias-Mac-mini-97.local>
2026-06-13 17:05:38 +0600

    fix: cleanup gitignore and gradle signing setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,11 +4,17 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+import java.util.Properties
+        import java.io.FileInputStream
 
 android {
     namespace = "com.example.saymymeds"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
+    val keystoreProperties = Properties()
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -34,7 +40,16 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
 }
